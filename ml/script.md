05af457826d5501b46e46255ab83b7f8cac16eb9
Clemens Tolboom <clemens@build2be.com>
2018-03-24 11:38:51 +0100

    Half baked ML move to bottom.

---
+++ ml/script.md
@@ -0,0 +1,19 @@
+# Machine Learning
+
+This is a simple maze game where a Machine learns to escape quickly.
+
+```ascii
++-------------+
+|      u      |
+|    l A r    |
+|      d      |
+|             |
+```
+
+The grid [x,y] contains an action object with change indicators for direction.
+
+action.up = 0.25
+action.down = 0.25
+action.left = 0.25
+action.right = 0.25
+
---
+++ ml/sketch.js
@@ -0,0 +1,191 @@
+var actions;
+var player;
+function setup() {
+  createCanvas(800, 800);
+
+  restart();
+}
+
+function restart() {
+  actions = new Actions(10,10);
+  player = new Player();
+}
+
+function mousePressed() {
+  restart();
+}
+
+function draw() {
+  background(255);
+  actions.draw();
+  player.draw();
+}
+
+class Player {
+  constructor() {
+    this.cols = actions.cols;
+    this.rows = actions.rows;
+
+    this.col = this.cols / 2;
+    this.row = 0;
+
+    this.w = width / this.cols;
+    this.h = height / this.rows;
+  }
+
+  draw() {
+    stroke(0,255, 0);
+    fill(0,0,255);
+    ellipse(this.col * this.w + this.w/2, this.row * this.h + this.h/2, 11, 11);
+    this.move();
+  }
+
+  move() {
+    let move = actions.takeMove(this.row, this.col);
+    let r = this.row;
+    let c = this.col;
+
+    let score = this['move_' + move]();
+
+    actions.updateAction(r,c,move, score);
+  }
+
+  // Policy
+  move_up() {
+    var score = 0;
+    if (this.row == 0) {
+      score = -1;
+    }
+    else {
+      this.row-=1;
+    }
+    return score;
+  }
+
+  move_down() {
+    var score = 0;
+    if (this.row === this.rows -1) {
+      score = 10;
+      this.row = 0;
+    }
+    else {
+      this.row+=1;
+    }
+    return score;
+  }
+
+  move_left() {
+    var score = 0;
+    if (this.col == 0) {
+      score = -1;
+    }
+    else {
+      this.col-=1;
+    }
+    return score;
+  }
+
+  move_right() {
+    var score = 0;
+    if (this.col == this.cols -1) {
+      score = -1;
+    }
+    else {
+      this.col+=1;
+    }
+    return score;
+  }
+
+
+}
+
+class Action {
+  constructor() {
+    // Sum === 1.0
+    this.up = 0.25;
+    this.down = 0.25;
+    this.left = 0.25;
+    this.right = 0.25;
+  }
+
+  draw(x,y) {
+    stroke(255,0, 0);
+    ellipse(x,y,5,5);
+
+    stroke(0);
+
+    line(x, y, x + this.right * 50, y)
+    line(x, y, x - this.left * 50, y)
+
+    line(x, y, x, y + this.down * 50)
+    line(x, y, x, y - this.up * 50)
+  }
+
+  take() {
+    var toss = random();
+    if (toss < this.up) {
+      return 'up';
+    }
+
+    if (toss < this.up + this.down) {
+      return 'down';
+    }
+
+    if (toss < this.up + this.down + this.left) {
+      return 'left';
+    }
+    return 'right';
+  }
+
+  update(move, score) {
+    this[move]+= score * 0.01;
+    this.normalize();
+  }
+
+  normalize() {
+    let sum = this.up + this.down +  this.left +  this.right;
+    this.up /= sum;
+    this.down /= sum;
+    this.left /= sum;
+    this.right /= sum;
+  }
+}
+
+class Actions {
+  constructor(x,y) {
+    this.rows = y;
+    this.cols = x;
+    var actions = new Array(y);
+    for( var row= 0; row< this.rows; row++) {
+      actions[row] = new Array(x);
+      for( var col= 0; col< this.cols; col++) {
+        actions[row][col] = new Action();
+      }
+    }
+    this.actions = actions;
+  }
+
+  takeMove(row, col) {
+    return this.actions[row][col].take();
+  }
+
+  updateAction(row,col,move, score) {
+    return this.actions[row][col].update(move, score);
+  }
+
+  draw() {
+    noFill();
+    let w = width/this.cols;
+    let h = height/this.rows;
+    for( var row= 0; row< this.rows; row++) {
+      for( var col= 0; col< this.cols; col++) {
+        let x = col*w + w/2;
+        let y = row*h + h/2
+        stroke(0, 0, 0);
+        rect(x - w/2, y - h/2, w-1, h-1);
+        let a = this.actions[row][col];
+        a.draw(x,y);
+      }
+    }
+  }
+}
